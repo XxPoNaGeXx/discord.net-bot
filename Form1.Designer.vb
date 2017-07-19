15642283477d391fd44d0d02cbdc3a08068b0c4b
KyleB <XxPoNaGeXx@users.noreply.github.com>
2017-07-19 22:29:13 +0100

    Source Code

---
+++ Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 301)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(195, 301)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Status"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(15, 33)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(388, 251)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(413, 59)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Send Message"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(413, 33)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(413, 96)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(413, 122)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Send TTS Message"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(413, 157)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(413, 183)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Set Game"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(413, 261)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Set Status"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Online", "Idle", "Do Not Disturb", "Invisible"})
+        Me.ComboBox1.Location = New System.Drawing.Point(413, 228)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(100, 21)
+        Me.ComboBox1.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(531, 323)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Sushi Bot"
+        Me.TransparencyKey = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,184 @@
+﻿Imports Discord
+
+Public Class Form1
+
+    Dim WithEvents Discord As New DiscordClient
+
+    Dim wClient As New System.Net.WebClient
+
+    Dim trigger As String = "?"
+
+    Dim admins() As String = {"Kyle", "blah", "blah1"}
+
+
+    Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+
+            Await Discord.Connect("MzM3MzE0ODE1NjA3MzA4MzAx.DFFEaQ.MwFepc3wsi7Po0HIMHcRXJViq4g", TokenType.Bot)
+
+            Await Task.Delay(500)
+
+            Label1.Text = Discord.CurrentUser.Name
+            getState()
+
+            ListBox1.Items.Add(Date.Now + " Successfully connected to Discord")
+
+        Catch ex As Exception
+
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+
+    End Sub
+
+
+    Private Async Sub OnMsg(sender As Object, message As Discord.MessageEventArgs) Handles Discord.MessageReceived
+
+        If message.User.Name = "Sushi" Then
+
+            'Ignore the Message
+
+        Else
+
+            Dim msg As String = message.Message.RawText
+
+            If msg.StartsWith(trigger) Then
+
+                If msg.Contains(" ") Then
+
+                    Dim cmd As String = msg.Split(trigger)(1).Split(" ")(0)
+                    Dim arg As String = msg.Split(" ")(1)
+
+                    Select Case cmd.ToLower
+
+                        Case "say"
+                            Await message.Channel.SendMessage(arg)
+
+                        Case Else
+                            Await message.Channel.SendMessage("Invalid Command.")
+
+                    End Select
+
+                Else
+
+                    Dim cmd As String = msg.Split(trigger)(1)
+
+                    Select Case cmd.ToLower
+
+                        Case "help"
+                            Await message.Channel.SendMessage("Commands:" + vbNewLine + "say - repeats the message")
+
+                        Case "joke"
+                            Await message.Channel.SendMessage(wClient.DownloadString("http://api.icndb.com/jokes/random"))
+
+                        Case "clear"
+                            Dim msgs() As Discord.Message = Await message.Channel.DownloadMessages(100)
+
+                            For Each mess As Discord.Message In msgs
+
+                                If mess.User.Name = Discord.CurrentUser.Name Then
+
+                                    Await mess.Delete()
+
+                                ElseIf mess.RawText.StartsWith(trigger) Then
+
+                                    Await mess.Delete
+
+                                End If
+
+
+                            Next
+
+                        Case "serverinfo"
+                            Await message.Channel.SendMessage("Server name: " + message.Server.Name + vbNewLine + "Members: " + CStr(message.Server.UserCount) + vbNewLine + "Roles: " + message.Server.RoleCount + vbNewLine + "Icon: " + message.Server.IconUrl)
+
+                        Case "troll"
+                            Await message.Channel.SendTTSMessage("Hello, is it me you're looking for?")
+
+
+
+                        Case Else
+                            Await message.Channel.SendMessage("Invalid Command.")
+
+                    End Select
+
+                End If
+
+            Else
+
+                'Ignore the message 
+
+            End If
+
+        End If
+
+    End Sub
+
+    Public Sub getState()
+
+        If Discord.State = ConnectionState.Connected Then
+
+            Label2.Text = "Connected"
+
+        ElseIf Discord.State = ConnectionState.Connecting Then
+
+            Label2.Text = "Connecting"
+        Else
+
+            Label2.Text = "Uknown"
+
+        End If
+
+    End Sub
+
+    Public Sub Welcomer(sender As Object, e As Discord.UserEventArgs) Handles Discord.UserJoined
+
+        Dim server = e.Server.FindChannels("general").FirstOrDefault()
+        server.SendMessage("Welcome to the server" + e.User.Name)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Discord.FindServers("KB").FirstOrDefault().FindChannels("sushi-bot").FirstOrDefault().SendMessage(TextBox1.Text)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Discord.FindServers("KB").FirstOrDefault().FindChannels("sushi-bot").FirstOrDefault().SendTTSMessage(TextBox2.Text)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Discord.SetGame(New Game(TextBox3.Text, GameType.Default, " "))
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        If ComboBox1.SelectedItem = "Idle" Then
+
+            Discord.SetStatus(UserStatus.Idle)
+
+        ElseIf ComboBox1.SelectedItem = "Online" Then
+
+            Discord.SetStatus(UserStatus.Online)
+
+        ElseIf ComboBox1.SelectedItem = "Do Not Disturb" Then
+
+            Discord.SetStatus(UserStatus.DoNotDisturb)
+
+        ElseIf ComboBox1.SelectedItem = "Invisible" Then
+
+            Discord.SetStatus(UserStatus.Invisible)
+
+        End If
+
+
+    End Sub
+End Class
